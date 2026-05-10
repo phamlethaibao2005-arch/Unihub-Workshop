f93afbaee798fd66a164292253cf945259994fd7
Jayce <pltbao23@clc.fitus.edu.vn>
2026-05-11 01:27:39 +0700

    Fix

--- shared/infrastructure/PrismaClient.ts
+++ shared/infrastructure/PrismaClient.ts
@@ -1,20 +1,14 @@
 import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "@prisma/client"
 
-const connectionString = process.env.DATABASE_URL
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL is required")
-}
-
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const db =
+export const db: PrismaClient =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter: new PrismaPg(connectionString),
+    adapter: new PrismaPg(process.env.DATABASE_URL!),
   })
 
 if (process.env.NODE_ENV !== "production") {
