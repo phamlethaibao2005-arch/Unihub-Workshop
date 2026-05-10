77e51cc7a40688900dd28f5281149db4925a0975
Jayce <pltbao23@clc.fitus.edu.vn>
2026-05-11 01:32:06 +0700

    Fix error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: [
+    "@prisma/client",
+    "@prisma/adapter-pg",
+    "better-auth",
+  ],
   images: {
     remotePatterns: [
       {
